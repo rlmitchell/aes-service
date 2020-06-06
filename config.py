916bfdceaf39d6acbbb4211c9255119206db0040
Rob Mitchell <rlmitchell@gmail.com>
2020-06-06 16:07:07 -0500

    config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import configparser
+
+__module__     = 'aes-service/config.py'
+__author__     = 'Rob Mitchell'
+__maintainer__ = 'Rob Mitchell'
+__email__      = 'rlmitchell@gmail.com'
+__version__    = '1.0.0'
+__status__     = 'personal use release'
+
+
+class Config(object):
+    __version__ = '1.0.0'
+    __version_date__ = '2020.06.06.1559ct'
+    parser = None
+
+    @staticmethod
+    def get_parser( file_path ):
+        if Config.parser is None:
+            Config.parser = configparser.ConfigParser()
+            Config.parser.read( file_path )
+        return Config.parser
+
