945eea4a9b95c38f6724ed7a9b304d7eb50ddc57
Rob Mitchell <rlmitchell@gmail.com>
2020-06-06 16:26:30 -0500

    config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 import configparser
 
-__module__     = 'aes-service/config.py'
-__author__     = 'Rob Mitchell'
-__maintainer__ = 'Rob Mitchell'
-__email__      = 'rlmitchell@gmail.com'
-__version__    = '1.0.0'
-__status__     = 'personal use release'
-
-
 class Config(object):
-    __version__ = '1.0.0'
+    __module__       = 'aes-service/config.py'
+    __author__       = 'Rob Mitchell'
+    __maintainer__   = 'Rob Mitchell'
+    __email__        = 'rlmitchell@gmail.com'
+    __version__      = '1.0.0'
     __version_date__ = '2020.06.06.1559ct'
+    __status__       = 'personal use release'
+
     parser = None
 
     @staticmethod
